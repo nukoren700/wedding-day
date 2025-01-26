ac3c9bda416e54ffee713b2470d745085ec5a66c
Nattapon Khansri <nattaponcomsci@gmail.com>
2025-01-26 15:26:34 +0700

    DB setting

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,33 @@
+// import mysql from 'mysql2/promise';
+
+// const pool = mysql.createPool({
+//   host: 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com', // ชื่อโฮสต์ของ MySQL
+//   user: 'pSqSMLCFhgheTbj.root',      // ชื่อผู้ใช้ MySQL
+//   password: 'YYge9nZgyww2zOxZ', // รหัสผ่าน MySQL
+//   database: 'test', // ชื่อฐานข้อมูล
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// export default pool;
+
 import mysql from 'mysql2/promise';
 
+// กำหนดค่าการเชื่อมต่อฐานข้อมูล
 const pool = mysql.createPool({
-  host: 'fdb1030.awardspace.net', // ชื่อโฮสต์ของ MySQL
-  user: '4579887_testdb',      // ชื่อผู้ใช้ MySQL
-  password: 'YYge9nZgyww2zOxZ', // รหัสผ่าน MySQL
-  database: '4579887_testdb', // ชื่อฐานข้อมูล
+  host: 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com', // เช่น xxx.tidbcloud.com
+  port: 4000, // Default port ของ TiDB
+  user: 'pSqSMLCFhgheTbj.root',
+  password: 'YYge9nZgyww2zOxZ',
+  database: 'test',
+  ssl: {
+    rejectUnauthorized: true, // ใช้ SSL สำหรับการเชื่อมต่อ
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
 export default pool;
+
